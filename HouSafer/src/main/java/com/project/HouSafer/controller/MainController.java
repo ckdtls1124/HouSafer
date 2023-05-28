b08b315754d21b825ed99501f4635e4764d77cdd
JavaBoy94 <o1w3l5@gmail.com>
2023-05-28 16:24:53 +0900

    메인, 피해사례 게시판(네이버 지도 api) 추가 (*samesite 처리필요)

---
+++ HouSafer/src/main/java/com/project/HouSafer/controller/MainController.java
@@ -0,0 +1,21 @@
+package com.project.HouSafer.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequiredArgsConstructor
+public class MainController {
+
+    @RequestMapping(value = {"/","/index"}, method = {RequestMethod.GET, RequestMethod.POST})
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping(value = "/fraudCases", method = {RequestMethod.GET, RequestMethod.POST})
+    public String fraudCases(){
+    return "fraudCases";
+    }
+}
